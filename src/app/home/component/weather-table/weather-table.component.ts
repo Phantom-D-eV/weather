2907f28519f079ef6350dabb9c48c5f238904361
Phantom-D-eV <german.kormakov@mail.ru>
2024-06-11 20:48:19 +0300

    table component

--- src/app/home/component/weather-table/weather-table.component.ts
+++ src/app/home/component/weather-table/weather-table.component.ts
@@ -1,11 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HomeService } from '../../home.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-weather-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './weather-table.component.html',
   styleUrl: './weather-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WeatherTableComponent {}
+export class WeatherTableComponent {
+  constructor(
+    protected route: ActivatedRoute,
+    protected readonly homeService: HomeService
+  ) {}
+
+  getHourly$ = this.homeService.getHourly();
+  getDaily$ = this.homeService.getDaily();
+}
